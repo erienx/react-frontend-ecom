0b852de162d026f40dca1dd4a085fdb27bc537ba
erienx <mat21mat12mat@gmail.com>
2025-05-24 12:18:09 +0200

    added wishlist link to the upper header

--- src/components/UpperHeader.tsx
+++ src/components/UpperHeader.tsx
@@ -1,16 +1,17 @@
 // import ProfileIcon from "../assets/profile-icon.svg?react"
 import { Link, useNavigate } from "react-router-dom"
 import CartIcon from "../assets/cart-icon.svg?react"
+import WishlistIcon from "../assets/wishlist-icon.svg?react"
 import { useAuth } from "../components/providers/AuthContext";
 import { useHasAccess } from "../hooks/useHasAccess";
 // import SearchIcon from "../assets/search-icon.svg?react"
 
 const UpperHeader = () => {
-    const {currentUser, handleLogout} = useAuth();
+    const { currentUser, handleLogout } = useAuth();
     const isAdmin = useHasAccess(['admin']);
     const navigate = useNavigate();
 
-    const logoutAndRedirect = async ( ) =>{
+    const logoutAndRedirect = async () => {
         await handleLogout();
         navigate('/');
     }
@@ -19,29 +20,37 @@ const UpperHeader = () => {
         <>
             <header className="flex flex-col sm:flex-row justify-end items-center px-4 sm:px-14 py-3 flex-wrap gap-y-12 sm:gap-y-8">
                 <div className="flex flex-wrap items-center justify-between gap-4 sm:gap-8 lg:gap-12">
-                {isAdmin && (<Link to='/admin'>Admin stuff</Link>)}
+                    {isAdmin && (<Link to='/admin'>Admin stuff</Link>)}
                     {currentUser ? (
                         <>
                             <span className="regular-text">Welcome, <span className="font-semibold">{currentUser.firstName + " " + currentUser.lastName}</span></span>
                             <button onClick={logoutAndRedirect} className="regular-text cursor-pointer">Sign Out</button>
                         </>
-                    ):
-                    (
-                        <>
-                            <Link to='register' className="regular-text">Register</Link>
-                            <Link to='login' className="regular-text">Sign in</Link>
-                        </>
-                    )}
+                    ) :
+                        (
+                            <>
+                                <Link to='register' className="regular-text">Register</Link>
+                                <Link to='login' className="regular-text">Sign in</Link>
+                            </>
+                        )}
                     {/* <div className="flex items-center gap-2">
                         <ProfileIcon />
                         <span className="regular-text">My Profile</span>
                     </div> */}
 
+                    {currentUser && (
+                        <div className="flex flex-row gap-3">
+                            <Link to='cart' className="flex items-center gap-2">
+                                <CartIcon />
+                                <span className="regular-text">Items</span>
+                            </Link>
+                            <Link to='wishlist' className="flex items-center gap-2">
+                                <WishlistIcon width={25} height={25} />
+                                <span className="regular-text">Wishlist</span>
+                            </Link>
 
-                    <Link to='cart' className="flex items-center gap-2">
-                        <CartIcon />
-                        <span className="regular-text">Items</span>
-                    </Link>
+                        </div>
+                    )}
 
                     {/* <div className="flex items-center gap-2">
                         <span className="regular-text">$0.00</span>
